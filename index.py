9d1e0961d03623ef083b68383f090ec49213d55a
Braden Vester <brvester@iupui.edu>
2024-04-28 17:56:10 -0400

    Final Changes

--- index.py
+++ index.py
@@ -129,11 +129,3 @@ def main():
     
 if __name__ == "__main__":
     main()
-    
-    
-# TODO: Add stats messages
-    
-# NOTE: On occasion, receive following error. Investigate.
-
-# raise ConnectionError(err, request=request)
-# requests.exceptions.ConnectionError: ('Connection aborted.', RemoteDisconnected('Remote end closed connection without response'))
